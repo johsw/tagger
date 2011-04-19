5dd213d000a2630e393318b7f30f14d786039aaa
wulff <wulff@ratatosk.net>
2011-04-19 13:59:20 +0200

    simplified allowed referer logic

--- access/iplog.php
+++ access/iplog.php
@@ -3,12 +3,20 @@
  * This is super lo-fi security, but try to disallow unwanted calls to the
  * webservice.
  */
+
 global $conf;
-if (!isset($_SERVER['HTTP_REFERER']) && !empty($conf['service_allow_referer'])) {
-  RestUtils::sendResponse(403);
+
+// If the allowed referers array is empty, we can skip the hostname check
+if (!empty($conf['service_allow_referer'])) {
+  // Get the hostname of the referer...
+  $host = '';
+  if (!empty($_SERVER['HTTP_REFERER'])) {
+    $referer = parse_url($_SERVER['HTTP_REFERER']);
+    $host = $referer['host'];
+  }
+
+  // ...and bail out if it's not in the list of allowed hostnames
+  if (!in_array($host, $conf['service_allow_referer'])) {
+    RestUtils::sendResponse(403);
+  }
 }
-$referer = parse_url($_SERVER['HTTP_REFERER']);
-if (!empty($conf['service_allow_referer']) && !in_array($referer['host'], $conf['service_allow_referer'])) {
-  RestUtils::sendResponse(403);
-}
-?>
