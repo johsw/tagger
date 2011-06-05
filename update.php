10aab04cc32251971571c3e9b7f7e42897c020a2
Johannes Wehner <johsw@information.dk>
2011-06-05 23:02:06 +0200

    First function definitions for the updater. Still no working code, just outlines.

--- update.php
+++ update.php
@@ -3,4 +3,12 @@
 require_once 'defaults.php';
 require_once 'conf.php';
 require_once 'textminer/Databasebuddy.inc.php';
-require_once 'updater/Updater.inc.php';
+require_once 'updater/Updater.class.php';
+
+/*
+* This file updates the datasets with data from a dataset server 
+*
+*/
+//TODO: Maybe check if file is run from cli or not?!
+
+$updater = new Updater();
--- updater/Updater.class.php
+++ updater/Updater.class.php
@@ -1,6 +1,53 @@
 <?php
 
 
+/**
+*  Updater class
+*/
 class Updater {
 
+  function __construct() {
+
+    $updates = $this->get_latest_updates(); 
+    $this->register_updates($updates);
+
+  }
+  
+  
+  public function get_latest_updates() {
+    global $conf;
+    $updates = array();
+    
+    if (!isset($conf['vocab_updater']) || empty($conf['vocab_updater'])) {
+      $this->update_error('No defined updaters. Check $conf[\'vocab_updater\'] in conf.php');
+    }
+    foreach ($conf['vocab_updater'] as $url => $options) {
+      if ($this->update_minimal_interval_passed($url)) {
+        $updates[$url] = fetch_remote_updates($url, $options);
+        # code...
+      }
+      
+    }
+    
+  }
+  public function update_minimal_interval_passed($url) {
+    # code...
+  }
+  
+  public function variables_table_exists() {
+    $result = DatabaseBuddy::query("SHOW TABLES like 'variables'");
+    print $result;
+  }
+  
+  public function fetch_remote_updates($url, $options) {
+    # code...
+  }
+  
+  public function update_error($error) {
+    print "\n";
+    print "-------\n";
+    print "ERROR: ". $error ."\n";
+    print "-------\n";
+    exit;
+  }
 }
