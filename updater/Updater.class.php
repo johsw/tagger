f5b0f27f9e13f964f647182b14122a0f4ccaef63
Johannes Wehner <johsw@information.dk>
2011-06-08 23:48:54 +0200

    Initial functionality for the updater class.

--- updater/Updater.class.php
+++ updater/Updater.class.php
@@ -9,8 +9,25 @@ class Updater {
 
   function __construct($updates) {
     global $conf;
-    print_r($conf);
-    print_r($updates); exit;
+    //print_r($conf);
+    $this->parse_updates($updates);
+    //print_r($updates); 
+    exit;
   }
   
+  public function parse_updates($updates) {
+    if (is_array($updates)) {
+      foreach ($updates as $uri => $set) {
+        if (is_array($set)) {
+          foreach ($set as $update) {
+            $sql = $this->build_sql($update);
+          }
+        }
+      }
+    }
+  }
+  
+  public function build_sql($update) {
+    
+  }
 }
