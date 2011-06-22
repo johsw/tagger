7d1be7a5ac0d8793776b10a416e977e07ebca58b
Johannes Wehner <johsw@information.dk>
2011-06-22 22:33:24 +0200

    Initial merge of the updater-branch.

---
+++ Update.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once 'defaults.php';
+require_once 'conf.php';
+require_once 'textminer/Databasebuddy.inc.php';
+require_once 'updater/Retriever.class.php';
+require_once 'updater/Updater.class.php';
+
+/*
+* This file updates the datasets with data from a dataset server 
+*
+*/
+//TODO: Maybe check if file is run from cli or not?!
+
+$retriever = new Retriever();
+$result = new Updater($retriever->updates);
---
+++ updater/Updater.class.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+*  Updater class
+*/
+
+
+class Updater {
+
+  function __construct($updates) {
+    global $conf;
+    //print_r($conf);
+    $this->parse_updates($updates);
+    //print_r($updates);
+    exit;
+  }
+
+  public function parse_updates($updates) {
+    if (is_array($updates)) {
+      foreach ($updates as $uri => $set) {
+        if (is_array($set)) {
+          foreach ($set as $update) {
+            $sql = $this->build_sql($update);
+          }
+        }
+      }
+    }
+  }
+
+  public function build_sql($update) {
+
+  }
+}
