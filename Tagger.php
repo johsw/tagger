5375c6198b5800d97fa2ef8689d44c88ac51e09f
Johannes Wehner <johsw@information.dk>
2012-03-16 19:46:02 +0100

    Some code formatting.
    The array_merge_recursive_simple function should probably be moved.

--- Tagger.php
+++ Tagger.php
@@ -110,7 +110,7 @@ class Tagger {
    *     ambigous tags.
    *   - 'return_uris': Boolean indicating wheter Tagger should return URI's for
    *     each tag
-   *   - 'log_unmatched': Boolean indicating whether unmatched potential 
+   *   - 'log_unmatched': Boolean indicating whether unmatched potential
    *     NER candidates should be logged
    *   - 'nl2br': Boolean indicating whether newlines should be convertet to br-tags
    * @return
@@ -176,31 +176,32 @@ $merge[0] === "foo"
 $merge[1] === "bar"
 */
 function array_merge_recursive_simple() {
-
-    if (func_num_args() < 2) {
-        trigger_error(__FUNCTION__ .' needs two or more array arguments', E_USER_WARNING);
-        return;
+  if (func_num_args() < 2) {
+    trigger_error(__FUNCTION__ .' needs two or more array arguments', E_USER_WARNING);
+    return;
+  }
+  $arrays = func_get_args();
+  $merged = array();
+  while ($arrays) {
+    $array = array_shift($arrays);
+    if (!is_array($array)) {
+      trigger_error(__FUNCTION__ .' encountered a non array argument', E_USER_WARNING);
+      return;
     }
-    $arrays = func_get_args();
-    $merged = array();
-    while ($arrays) {
-        $array = array_shift($arrays);
-        if (!is_array($array)) {
-            trigger_error(__FUNCTION__ .' encountered a non array argument', E_USER_WARNING);
-            return;
+    if (!$array) {
+      continue;
+    }
+    foreach ($array as $key => $value) {
+      if (is_string($key)) {
+        if (is_array($value) && array_key_exists($key, $merged) && is_array($merged[$key])) {
+          $merged[$key] = call_user_func(__FUNCTION__, $merged[$key], $value);
+        } else {
+          $merged[$key] = $value;
         }
-        if (!$array)
-            continue;
-        foreach ($array as $key => $value)
-            if (is_string($key))
-                if (is_array($value) && array_key_exists($key, $merged) && is_array($merged[$key]))
-                    $merged[$key] = call_user_func(__FUNCTION__, $merged[$key], $value);
-                else
-                    $merged[$key] = $value;
-            else
-                $merged[] = $value;
+      } else {
+        $merged[] = $value;
+      }
     }
-    return $merged;
+  }
+  return $merged;
 }
-
-
