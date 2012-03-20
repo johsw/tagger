8351af0a105baeef343962a59c2c281a5641558b
Johs. Wehner <johsw@information.dk>
2012-03-20 23:10:54 +0100

    Fix a small bug, that left out multi-word entities.

--- classes/EntityPreprocessor.class.php
+++ classes/EntityPreprocessor.class.php
@@ -1,7 +1,5 @@
 <?php
-
 require_once __ROOT__ . 'classes/Token.class.php';
-
 class EntityPreprocessor {
   private $tokens;
   private $named_entities;
@@ -23,21 +21,29 @@ class EntityPreprocessor {
    * Simply attempts to attract place names or names.
    */
   private function extract_potential_named_entities() {
+
     for ($i = 0, $n = count($this->tokens); $i < $n; $i++) {
       $entity = NULL;
       $token = $this->tokens[$i];
 
       // If the token is uppercase, maybe it is a name or a place.
       if ($token->isUpperCase() && !$token->isStopWord() && !$token->isInitWord()) {
-        $entity = array($i => &$this->tokens[$i]);
+        $entity = array(&$this->tokens[$i]);
 
-        // If the next token is not a punctuation break
-        while (!(isset($this->tokens[$i+1]) && preg_match('/[!\?,:;\(\)]/u', $this->tokens[$i+1]->text))
-          && isset($this->tokens[$i+2])
-          && ($this->tokens[$i+2]->isUpperCase() || $this->tokens[$i+2]->isPrefixOrInfix())
-          && $this->tokens[$i+2]->paragraphNumber == $token->paragraphNumber) {
+        /*
+         * If the next token is not a punctuation break
+         * and it's uppercase or is an infix
+         * and it's on the same paragraph
+         * then add it to the potential entity
+         *
+         */
 
-          $entity[$i] = &$this->tokens[$i+2];
+        while (!(isset($this->tokens[$i + 1]) && preg_match('/[!\?,:;\(\)]/u', $this->tokens[$i + 1]->text))
+          && isset($this->tokens[$i + 2])
+          && ($this->tokens[$i + 2]->isUpperCase() || $this->tokens[$i + 2]->isPrefixOrInfix())
+          && $this->tokens[$i + 2]->paragraphNumber == $token->paragraphNumber
+        ) {
+          $entity[] = &$this->tokens[$i + 2];
           $i += 2;
         }
         $this->named_entities[] = $entity;
