42b8260c32441f570a7e0a1667f62b40807bee48
Malthe Jørgensen <malthe.banan@gmail.com>
2012-04-24 16:46:09 +0200

    Add CLI for Install.php
    -f option for config file
    -j option for JSON loading

--- Install.php
+++ Install.php
@@ -3,6 +3,41 @@
 require_once('Tagger.php');
 require_once __ROOT__ . 'classes/TaggerInstaller.class.php';
 
-$tagger = Tagger::getTagger();
+/***
+ * 
+ * Called by itself this file will create the Tagger database tables.
+ * Called with the flag '-j' (json) it will fill the tables
+ * with data specified in the json files.
+ */
 
+$run_json = FALSE;
+
+// If called from command line
+if (php_sapi_name() == 'cli') {
+  $cargs = getopt('jf::');
+
+  if (isset($cargs['f'])) {
+    if (file_exists($cargs['f'])) {
+      $file = $cargs['f'];
+      $tagger = Tagger::getTagger(array(), $file);
+    }
+    else {
+      die('Config file does not exist:' . $cargs['f']);
+    }
+  }
+
+  if (isset($cargs['j'])) {
+    $run_json = TRUE;
+  }
+}
+
+$tagger = Tagger::getTagger();
 $install = new TaggerInstaller($tagger);
+
+if ($run_json) {
+  require_once __ROOT__ . 'classes/KeywordImporter.class.php';
+  $KI = new KeywordImporter();
+  $KI->jsonCreateKeywords(__ROOT__ . 'keywords.json');
+  $KI->jsonCreateWordstats(__ROOT__ . 'keyword_texts.json');
+  $KI->jsonCreateWordRelations(__ROOT__ . 'keyword_texts.json');
+}
