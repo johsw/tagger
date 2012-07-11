b667fe71df74904dd7484434bed1ff13c7c12313
Malthe Jørgensen <malthe.banan@gmail.com>
2012-07-11 15:27:06 +0200

    Documentation for Disambiguator.class.php

--- classes/Disambiguator.class.php
+++ classes/Disambiguator.class.php
@@ -1,9 +1,32 @@
 <?php
+/**
+ * @file
+ * Definition of Disambiguator.
+ */
 
+/**
+ * Contains disambiguation functionality.
+ *
+ * When the NamedEntityMatcher finds multiple matches (Tags) for a word in the
+ * text it returns a Tag with multiple meanings ($tag->meanings).
+ * For example "America" is both a band name, name of several cities, a
+ * supercontinent, colloquial name of a country and so on...
+ * The Disambiguator uses the disambiguation table in the database to find
+ * related words to each of these `meanings`, and then picks the meaning with
+ * most related words occurring in the text.
+ *
+ * This functionality is relevant because different `meanings` is really
+ * different Tags with different tids and different linked data. And so to
+ * return the correct linked data, disambigutation must occur.
+ */
 class Disambiguator {
+
 
   private $tags;
 
+  /**
+   * Constructs a Disambiguator object.
+   */
   public function __construct($tags, $text) {
     $this->tags = $tags;
     $this->tag_ids = array();
@@ -14,6 +37,13 @@ class Disambiguator {
       }
     }
   }
+
+  /**
+   * Disambiguation function.
+   *
+   * Runs through each ambiguous tag (those that have multiple meanings) and
+   * finds the most probable meaning and set the tid accordingly.
+   */
   public function disambiguate() {
 
     if (!isset($this->tags)) {
@@ -34,6 +64,19 @@ class Disambiguator {
     return $this->tags;
   }
 
+  /**
+   * Disambiguates a single Tag.
+   *
+   * For each meaning of the Tag it fetches the related words (via
+   * getRelatedWords) and chooses the meaning with the most related words
+   * occurring in the text.
+   *
+   * @param Tag $tag
+   *   The that should be disambiguated.
+   *
+   * @return int
+   *   The tid of meaning the with most occurences of related words in the text.
+   */
   public function checkRelatedWords($tag) {
     $related_words = $this->getRelatedWords($tag);
     $max_matches = 0;
@@ -42,7 +85,7 @@ class Disambiguator {
     foreach ($related_words as $tid => $words) {
       $subtotal = 0;
       foreach ($words as $word) {
-        
+
         //Check for each related word how many times it occurs in the text
         $subtotal += substr_count($this->text, $word);
         if($subtotal > $max_matches) {
@@ -53,6 +96,17 @@ class Disambiguator {
     }
     return $current;
   }
+
+  /**
+   * Gets related words from the disambigutation table.
+   *
+   * @param Tag $tag
+   *   The tag for which to find related words.
+   *
+   * @return array
+   *   An associative array with tids as keys and values that are arrays of
+   *   words related to each tid.
+   */
   public function getRelatedWords($tag) {
     $tagger_instance = Tagger::getTagger();
     $db_conf = $tagger_instance->getConfiguration('db');
@@ -69,15 +123,22 @@ class Disambiguator {
     return $matches;
   }
 
+  /**
+   * Get the vocabulary for a particular tag.
+   *
+   * Gets the vid (vocabulary id) for specific tid (term id) in the database.
+   *
+   * @param int $tid
+   *   The tid for which to find the vid.
+   */
   public function getVocabulary($tid) {
-    $tagger_instance = Tagger::getTagger();
-    $db_conf = $tagger_instance->getConfiguration('db');
+    $db_conf = Tagger::getConfiguration('db');
     $lookup_table = $db_conf['lookup_table'];
 
     $sql = sprintf("SELECT c.vid FROM $lookup_table AS c WHERE c.tid = %s LIMIT 0,1", $tid);
     $result = TaggerQueryManager::query($sql);
     $row = TaggerQueryManager::fetch($result);
-    $vocab_names = $tagger_instance->getConfiguration('vocab_names');
+    $vocab_names = Tagger::getConfiguration('vocab_names');
     return $row['vid'];
   }
 }
