baae59d5741cbeefb9e9ae7a248a235c58267f71
Malthe Jørgensen <malthe.banan@gmail.com>
2012-07-11 14:56:58 +0200

    Documentation for Token.class.php

--- classes/Token.class.php
+++ classes/Token.class.php
@@ -1,61 +1,136 @@
 <?php
+/**
+ * @file
+ * Definition of Token.
+ */
+
+
 /*
- * A token points to a single word or a small number of consecutive words at a
- * specific point in the text
+ * Represents a token.
+ *
+ * A single word or a small number of consecutive words.
  */
 class Token {
 
-  static $initwords;
-  static $prefix_infix;
-  static $stopwords;
 
+  /**
+   * The text contained in the token.
+   */
   public $text;
+
+  /**
+   * The text contained in the token. (lowercase)
+   */
   public $text_lowercase;
+
+  /**
+   * Tokens that form this token.
+   */
   public $tokenParts = array();
 
+  /**
+   * The total rating of this token.
+   */
   public $rating = 0;
+
+  /**
+   * The positional rating of this token.
+   */
   public $posRating = 1;
-  public $freqRating = 1;
+
+
+  /**
+   * The HTML rating of this token.
+   */
   public $htmlRating = 0;
 
+  /**
+   * The token number of this token in the text.
+   */
   public $tokenNumber = NULL;
+
+  /**
+   * The paragraph which this token is in.
+   */
   public $paragraphNumber = NULL;
 
+  /**
+   * Whether the tag has been modified for highlighting.
+   */
   public $hasBeenHighlighted = FALSE;
 
 
+  /**
+   * Construct a token object.
+   *
+   * @param string $text
+   *   The text of the token.
+   */
   public function __construct($text) {
-    $language = Tagger::getConfiguration('language');
-
     $this->text = $text;
     $this->text_lowercase = mb_strtolower($this->text, 'UTF-8');
-
   }
 
-  public function getText() {
-    return $this->text;
-  }
-
+  /**
+   * Whether this token is uppercase.
+   *
+   * @return bool
+   *   TRUE if the token is uppercase.
+   */
   public function isUpperCase() {
     return $this->text != $this->text_lowercase;
   }
 
+  /**
+   * Whether this token is a stopword.
+   *
+   * @return bool
+   *   TRUE if the token is a stopword.
+   */
   public function isStopWord() {
     return isset(Tagger::$stopwords[$this->text_lowercase]);
   }
 
+  /**
+   * Whether this token is an initword.
+   *
+   * @return bool
+   *   TRUE if the token is an init word.
+   */
   public function isInitWord() {
     return isset(Tagger::$initwords[$this->text_lowercase]);
   }
 
+  /**
+   * Whether this token is an infix or prefix word.
+   *
+   * @return bool
+   *   TRUE if the token is an infix or prefix word.
+   */
   public function isPrefixOrInfix() {
     return isset(Tagger::$prefix_infix[$this->text_lowercase]);
   }
 
+  /**
+   * Whether this token is an infix or prefix word.
+   *
+   * @return string
+   *   The text of the Token ($text).
+   */
   public function __toString() {
     return $this->text;
   }
 
+  /**
+   * Rates the token.
+   *
+   * A product of the positional and HTML rating of this token.
+   *
+   * @param int $token_count
+   *   The total number of tokens in the text.
+   * @param int paragraph_count
+   *   The total number of paragraphs in the text.
+   */
   public function rateToken($token_count, $paragraph_count) {
     $rating = Tagger::getConfiguration('named_entity', 'rating');
     $min_pos_rating = (1 - $rating['positional_minimum']) * exp(-$token_count/$rating['positional_critical_token_count']) + $rating['positional_minimum'];
@@ -81,3 +156,4 @@ class Token {
   }
 
 }
+
