259ed9410200be5015fec5afc74700f78877f1d1
Malthe Jørgensen <malthe.banan@gmail.com>
2012-07-11 15:27:27 +0200

    Documentation for Tag.class.php

--- classes/Tag.class.php
+++ classes/Tag.class.php
@@ -1,24 +1,57 @@
 <?php
+/**
+ * @file
+ * Definition of Tag.
+ */
 
 require_once __ROOT__ . 'classes/Token.class.php';
 
 /*
+ * Represents a token.
+ *
  * Tags are the overall tags found in the text, i.e. tags are unique.
- * The tag points to the specific places in the text where it was found ($tokens)
- * and has a rating that is the sum of the ratings of the individual tokens.
+ * The tag is a collection of identical tokens found at different places in the 
+ * text.
+ * It has a rating that is the sum of the ratings of the individual tokens.
  */
 class Tag extends Token {
 
+  /**
+   * The tokens in the text matched by this tag.
+   */
   public $tokens = array();
 
+  /**
+   * Canonical name of entity (as found in database).
+   */
   public $realName;
+
+  /**
+   * Names for this tag found in the text.
+   */
   public $synonyms = array();
 
+  /**
+   * Positional rating of this tag.
+   */
   public $posRating = 0;
+
+  /**
+   * The frequency rating of this tag.
+   */
   public $freqRating = 0;
 
+  /**
+   * Whether this tag is a named entity or a keyword.
+   */
   public $type = 'named_entity'; // keyword, named_entity
 
+  /**
+   * Constructs a Tag object.
+   *
+   * @param string|Token $token
+   *   A string or Token that contains the name of this tag.
+   */
   public function __construct($token) {
     if (is_string($token)) {
       parent::__construct($token);
@@ -35,6 +68,11 @@ class Tag extends Token {
     }
   }
 
+  /**
+   * Rates the tag.
+   *
+   * Sums up the ratings of the Tag's Tokens.
+   */
   public function rate() {
     foreach ($this->tokens as $synonym_tokens) {
       foreach ($synonym_tokens as $token) {
