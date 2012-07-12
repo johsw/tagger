0d562443591925cf922026fa0cbb5c632500acad
Malthe Jørgensen <malthe.banan@gmail.com>
2012-07-12 15:52:04 +0200

    Documentation for Tokenizer.class.php

--- classes/Tokenizer.class.php
+++ classes/Tokenizer.class.php
@@ -1,25 +1,69 @@
 <?php
+/**
+ * @file
+ * Definition of Tokenizer.
+ */
 
 require_once __ROOT__ . 'classes/Token.class.php';
 
+/**
+ * Implements tokenizing functionality.
+ *
+ * This class contains function to split texts into tokens.
+ */
 class Tokenizer {
-  public $words;
+  /**
+   * When the text is split via the constructor of Tokenizer, the `Token`s are
+   * saved in this array.
+   */
   public $tokens;
 
-  public function __construct($text, $capture = false) {
+  /**
+   * This regex defines the delimiters at which the text is split.
+   *
+   * The text is split at whichever places in the text this regex matches.
+   * The tokens are whatever is between these delimiters (spaces,
+   * commas, quotes etc.)
+   */
+  public static $split_regex = "/('s|[\s!\?,\.:;'\"«»\(\)]+)/u";
+
+  /**
+   * Splits a text into `Token`s
+   *
+   * @param string $text
+   *   The text to be split.
+   * @param bool $capture
+   *   Whether delimiters (comma, period, colon etc.) should be returned along
+   *   with the tokens.
+   */
+  public function __construct($text, $capture = FALSE) {
 
     $text = str_replace("\n", " __newline__ ", $text);
     $text = str_replace("\r", " __newline__ ", $text);
-    $this->words = $this->split_words($text, $capture);
-    foreach($this->words as $word) {
+    $words = $this->split_words($text, $capture);
+    foreach($words as $word) {
       if($word != '') {
         $this->tokens[] = new Token($word);
       }
     }
   }
 
-  public static $split_regex = "/('s|[\s!\?,\.:;'\"«»\(\)]+)/u";
-  public static function split_words($text, $capture = false) {
+  /**
+   * The splitting function.
+   *
+   * Uses `preg_split` and `$split_regex` to split a text into words.
+   *
+   * @param string $text
+   *   The text to be split
+   * @param bool $capture
+   *   Whether delimiters (comma, period, colon etc.) should be returned along
+   *   with the tokens.
+   *
+   * @return array
+   *   An array of strings which are the tokens and possibly
+   *   (depending on $capture) the delimiters.
+   */
+  public static function split_words($text, $capture = FALSE) {
     if ($capture) {
       return preg_split(self::$split_regex, $text, -1, PREG_SPLIT_DELIM_CAPTURE);
     }
