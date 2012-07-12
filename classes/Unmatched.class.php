e1c92dcd3097b0df2a4038b55bfa77d7aa43f73b
Malthe Jørgensen <malthe.banan@gmail.com>
2012-07-12 15:52:29 +0200

    Documentation for Unmatched.class.php

--- classes/Unmatched.class.php
+++ classes/Unmatched.class.php
@@ -1,14 +1,32 @@
 <?php
+/**
+ * @file
+ * Definition of Unmatched.
+ */
 
+/**
+ * Functionality for logging of Unmatched words.
+ */
 class Unmatched {
 
+  /**
+   * Array of unmatched words.
+   */
   private $unmatched;
 
-
+  /**
+   * Constructs an Unmatched object.
+   */
   public function __construct($unmatched) {
     $this->unmatched = $unmatched;
   }
 
+  /**
+   * Logs all unmatched words.
+   *
+   * Also increments a counter of how many times it has been logged as
+   * unmatched.
+   */
   public function logUnmatched() {
     foreach($this->unmatched as $word) {
       if (!empty($word)) {
@@ -21,6 +39,9 @@ class Unmatched {
     }
   }
 
+  /**
+   * Checks the database whether the word already is in the unmatched_table.
+   */
   private function wordHasBeenLogged($word) {
     $tagger = Tagger::getTagger();
     $db_conf = $tagger->getConfiguration('db');
@@ -31,10 +52,18 @@ class Unmatched {
     $row = TaggerQueryManager::fetch($result);
     return (bool)$row['count'];
   }
+
+  /**
+   * Insert a word into the unmatched_table.
+   */
   private function logNewWord($word) {
     $sql = sprintf("INSERT INTO $unmatched_table SET name = '%s', count = 1, created = CURRENT_TIMESTAMP", $word);
     $result = TaggerQueryManager::query($sql);
   }
+
+  /**
+   * Increments the word's count in the unmatched_table.
+   */
   private function incrementWordCount($word) {
     $sql = sprintf("UPDATE $unmatched_table SET count = count+1, updated = CURRENT_TIMESTAMP WHERE name ='%s';", $word);
     $result = TaggerQueryManager::query($sql);
